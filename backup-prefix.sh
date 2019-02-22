f8abb8bbf7c35de1f830b7397c803a686ae6f262
chz100p <chz100p@live.jp>
2019-02-22 10:24:54 +0900

    Add files via upload

--- backup-prefix.sh
+++ backup-prefix.sh
@@ -1,14 +1,16 @@
+#
+
 #sh /sdcard/Download/start-bash.sh
 
 root="/data/data/jackpal.androidterm/r"
 txz="/sdcard/Download/backup-prefix-$(date '+%Y%m%d%H%M%S').txz"
 
-( cd "${root}" && \
+cd "${root}" && \
 find . \
 -path ./data/gentoo/usr/portage -print0 -prune -o \
 -path ./data/gentoo/tmp -print0 -prune -o \
 -path ./home/chz100p/workspace -print0 -prune -o \
--print0 ) | \
-tar c -v --xz --no-recursion --null -T - -f "${txz}"
+-print0 | \
+tar c -v --no-recursion --null -T - -f - | xz -z > "${txz}"
 
-#tar x -C "${root}" -f "${txz}"
+#tar x -v -C "${root}" -f "${txz}"
